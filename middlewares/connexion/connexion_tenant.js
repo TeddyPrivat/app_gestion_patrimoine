46a628e09726a8b9be46cf18374c92092b1b386f
TeddyPrivat <47928474+TeddyPrivat@users.noreply.github.com>
2025-04-25 16:43:12 +0200

    toutes les créations et connexions de user sont rétablies sans bcrypt

--- middlewares/connexion/connexion_tenant.js
+++ middlewares/connexion/connexion_tenant.js
@@ -1,22 +1,23 @@
-const bcrypt = require('bcryptjs');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const cryptojs = require('cryptojs');
 
 async function signInTenant(email, password) {
+  const emailHashed = cryptojs.Crypto.SHA256(email.toLowerCase()).toString();
+  console.log("emailHashed", emailHashed);
   const tenant = await prisma.tenant.findUnique({
-    where:{ email: email.toLowerCase() },
+    where:{ emailHash: emailHashed },
     include: { users: true },
   });
-  
+
   if (!tenant) {
     throw new Error('Email incorrect ou utilisateur non trouvé');
+  }else{
+    if(tenant.password !== cryptojs.Crypto.SHA256(password).toString()) {
+      console.log(cryptojs.Crypto.SHA256(password).toString());
+      throw new Error('Mot de passe incorrect');
+    }
   }
-
-  const isValid = await bcrypt.compare(password, tenant.password);
-  if (!isValid) {
-    throw new Error('Mot de passe incorrect');
-  }
-
   return tenant;
 }
 
